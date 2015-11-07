2ad58499b147be3b81a54572d5fdfdbab73e12ef
Gary Gipson <ggipson@localhost.localdomain>
2015-11-07 16:59:30 -0600

    ADDED:  Facebook / Twitter / Youtube links

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -7,11 +7,11 @@
         </div>
         <div class="col-md-4">
           <ul class="copyright-links list-inline">
-            <li><a href="#fakelink"><i class="fa fa-facebook fa-lg"></i></a> </li>
-            <li><a href="#fakelink"><i class="fa fa-twitter fa-lg"></i></a> </li>
-            <li><a href="#fakelink"><i class="fa fa-youtube-play fa-lg"></i></a> </li>
-            <li><a href="#fakelink"><i class="fa fa-google-plus fa-lg"></i></a> </li>
-            <li><a href="#fakelink"><i class="fa fa-instagram fa-lg"></i></a> </li>
+            <li><a href="https://www.facebook.com/JV-Equipment-Inc-100276043359234/" target="_blank"><i class="fa fa-facebook fa-lg"></i></a> </li>
+            <li><a href="https://twitter.com/jvequipment" target="_blank"><i class="fa fa-twitter fa-lg"></i></a> </li>
+            <li><a href="https://www.youtube.com/user/jvequipment" target="_blank"><i class="fa fa-youtube-play fa-lg"></i></a> </li>
+            {{--<li><a href="#fakelink"><i class="fa fa-google-plus fa-lg"></i></a> </li>
+            <li><a href="#fakelink"><i class="fa fa-instagram fa-lg"></i></a> </li>--}}
           </ul>
         </div>
       </div>
